da76308b4db2b9eac747967958274c6dcba81766
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2025-10-17 01:12:33 +0700

    config mysql and test query

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,32 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend running successfully 🚀");
 });
+
+// test database connection
+const checkDBConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL connected!");
+    connection.release();
+  } catch (error) {
+    console.error("MySQL connection failed:", error.message);
+  }
+};
+checkDBConnection();
+
+// test quẻy
+app.get("/test-users", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM users LIMIT 10");
+    console.log(rows);
+    res.json(rows);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Query failed" });
+  }
+});
+
+
 
 const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
