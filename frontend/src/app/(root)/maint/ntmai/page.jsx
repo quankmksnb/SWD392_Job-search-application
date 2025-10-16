df66e7f47cb3f58890f4d3196d36006f7dc50268
Nguyễn Minh Quân <quannguyenkm16122004@gmail.com>
2025-10-16 22:56:22 +0700

    clear

--- frontend/src/app/(root)/maint/ntmai/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-[1000px]">
-      <h1 className="text-[70px]">test con của mai</h1>
-    </div>
-  );
-}
--- frontend/src/app/(root)/maint/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-[1000px]">
-      <h1 className="text-[70px]">Test URL Home không có header footer</h1>
-    </div>
-  );
-}
